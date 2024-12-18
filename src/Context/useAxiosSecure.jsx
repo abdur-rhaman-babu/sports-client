0565a4d15ea5ddacaca442e1d88f89b934e441cc
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-18 12:26:13 +0600

    implement axios interceptors

--- src/Context/useAxiosSecure.jsx
+++ src/Context/useAxiosSecure.jsx
@@ -1,11 +1,36 @@
 import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:2500',
-    withCredentials: true,
-})
+  baseURL: "http://localhost:2500",
+  withCredentials: true,
+});
 const useAxiosSecure = () => {
-   return axiosInstance;    
+  const { logoutUser } = useContext(AuthContext);
+  const navigate = useNavigate()
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.status === 401 || error.status === 403) {
+            logoutUser()
+            .then(()=>{
+                console.log('logout user')
+                navigate('/login')
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+  return axiosInstance;
 };
 
 export default useAxiosSecure;
